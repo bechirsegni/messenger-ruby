896e4f38efaef682c390be2ad6bf5201d919f919
mejcz <magda.wisniewska@netguru.co>
2016-04-18 23:31:21 +0200

    Move messenger to messenger-ruby

---
+++ lib/messenger-ruby.rb
@@ -0,0 +1,20 @@
+require "messenger/version"
+require "messenger/configuration"
+
+module Messenger
+  class << self
+    attr_accessor :config
+
+    def config
+      @config ||= Configuration.new
+    end
+
+    def configure
+      yield(config)
+    end
+
+    def reset
+      @config = Configuration.new
+    end
+  end
+end
