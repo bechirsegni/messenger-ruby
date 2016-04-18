614ae60c283ee5e8cf29680b93df273c25def8f6
justynjozwiak <justyn.jozwiak@netguru.pl>
2016-04-18 16:05:28 +0200

    update configuration spec

--- spec/messenger/configuration_spec.rb
+++ spec/messenger/configuration_spec.rb
@@ -13,5 +13,21 @@ module Messenger
         expect(Configuration.new.page_access_token).to be nil
       end
     end
+
+    describe '#verify_token=' do
+      it 'sets verify_token properly' do
+        config = Configuration.new
+        config.verify_token = 'some token'
+        expect(config.verify_token).to eql 'some token'
+      end
+    end
+
+    describe '#page_access_token=' do
+      it 'sets page_access_token properly' do
+        config = Configuration.new
+        config.page_access_token = 'some token'
+        expect(config.page_access_token).to eql 'some token'
+      end
+    end
   end
 end
