3cf524ee4a7e264f4b6b61ee4d33f745356c8fd1
mejcz <magda.wisniewska@netguru.co>
2016-04-19 14:31:53 +0200

    Make sure that not published page will be active after successful token validation

--- app/controllers/messenger/messenger_controller.rb
+++ app/controllers/messenger/messenger_controller.rb
@@ -18,15 +18,22 @@ module Messenger
 
     private
 
+    def app_location
+      "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=#{Messenger.config.page_access_token}"
+    end
+
     def activate_bot
-      RestClient.post(
-        "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=#{Messenger.config.page_access_token}",
-        nil
-      )
+      JSON.parse(RestClient.post(app_location, nil)).key?('success')
+    end
+
+    def page_published?
+      JSON.parse(RestClient.get(app_location))['data'].any?
+    rescue RestClient::BadRequest
+      return false
     end
 
     def bot_active?
-      JSON.parse(activate_bot).key?('success')
+      page_published? || activate_bot
     end
 
     def valid_token?
