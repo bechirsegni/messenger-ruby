d69bdfb1709af1328089a51afe6639f3ee4765e6
mejcz <magda.wisniewska@netguru.co>
2016-04-19 13:09:12 +0200

    Send request to Facebook graph to activate bot

--- app/controllers/messenger/messenger_controller.rb
+++ app/controllers/messenger/messenger_controller.rb
@@ -2,7 +2,8 @@ module Messenger
   class MessengerController < ApplicationController
     def validate
       if params["hub.verify_token"] == Messenger.config.verify_token
-         render json: params["hub.challenge"]
+        render json: params["hub.challenge"]
+        activate_bot
       else
         render json: 'Error, wrong validation token'
       end
@@ -10,6 +11,15 @@ module Messenger
 
     def webhook
       #here user will be able to define bot behaviour
+    end
+
+    private
+
+    def activate_bot
+      RestClient.post(
+        "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=#{Messenger.config.page_access_token}",
+        nil
+      )
     end
   end
 end
