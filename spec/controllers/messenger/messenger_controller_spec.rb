b20179f6d5e2bfa5258a77bc9fb263b64f9ec3e2
mejcz <magda.wisniewska@netguru.co>
2016-04-19 17:39:15 +0200

    Test scenario with already activated bot and correct verify_token

--- spec/controllers/messenger/messenger_controller_spec.rb
+++ spec/controllers/messenger/messenger_controller_spec.rb
@@ -3,5 +3,26 @@ require "spec_helper"
 module Messenger
   describe MessengerController, type: :controller do
     routes { Messenger::Engine.routes }
+
+    let(:facebook_params) do
+      { 'hub.verify_token' => "TEST_TOKEN", 'hub.challenge' => 'test_challenge' }
+    end
+
+    describe 'get #validate' do
+      describe 'bot is already activated' do
+        before do
+          described_class.any_instance.stub(:bot_active?).and_return(true)
+        end
+
+        describe 'verify token is correct' do
+          before { Messenger.config.verify_token = "TEST_TOKEN" }
+
+          it 'should return `hub.challenge`' do
+            get :validate, facebook_params
+            expect(response.body).to eq('test_challenge')
+          end
+        end
+      end
+    end
   end
 end
