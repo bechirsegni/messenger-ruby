0a3591ad7f5032f31659da68433c47d8a7f2762f
mejcz <magda.wisniewska@netguru.co>
2016-04-19 13:44:26 +0200

    Handle invalid page access token

--- app/controllers/messenger/messenger_controller.rb
+++ app/controllers/messenger/messenger_controller.rb
@@ -1,11 +1,14 @@
+require 'rest-client'
+
 module Messenger
   class MessengerController < ApplicationController
     def validate
-      if params["hub.verify_token"] == Messenger.config.verify_token
+      if valid_token? && bot_active?
         render json: params["hub.challenge"]
-        activate_bot
+      elsif valid_token?
+        render json: 'Invalid page access token'
       else
-        render json: 'Error, wrong validation token'
+        render json: 'Invalid verify token'
       end
     end
 
@@ -20,6 +23,14 @@ module Messenger
         "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=#{Messenger.config.page_access_token}",
         nil
       )
+    end
+
+    def bot_active?
+      JSON.parse(activate_bot).key?('success')
+    end
+
+    def valid_token?
+      params["hub.verify_token"] == Messenger.config.verify_token
     end
   end
 end
