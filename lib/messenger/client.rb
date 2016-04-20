205b0982029f0d8404868cec52a93ad56b4c71fd
mejcz <magda.wisniewska@netguru.co>
2016-04-20 10:31:32 +0200

    Add client for messenger API - allow get user profile and send message

---
+++ lib/messenger/client.rb
@@ -0,0 +1,18 @@
+module Messenger
+  class Client
+    def self.get_user_profile(user_id)
+      RestClient.get(
+        "https://graph.facebook.com/v2.6/#{user_id}?fields=first_name,last_name,profile_pic \
+        &access_token=#{Messenger.config.page_access_token}"
+      )
+    end
+
+    def self.send(data)
+      RestClient.post(
+        "https://graph.facebook.com/v2.6/me/messages?access_token=#{Messenger.config.page_access_token}",
+        data,
+        content_type: :json
+      )
+    end
+  end
+end
