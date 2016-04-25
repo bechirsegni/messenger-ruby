21f5825fe9fef27f0f60bc3ddd7a8c76bc80ccb5
mejcz <magda.wisniewska@netguru.co>
2016-04-25 17:30:44 +0200

    Allow`location`as attachment

--- lib/messenger/parameters/attachment.rb
+++ lib/messenger/parameters/attachment.rb
@@ -1,11 +1,22 @@
 module Messenger
   module Parameters
     class Attachment
-      attr_accessor :type, :url
+      attr_accessor :type, :url, :long, :lat
 
       def initialize(type:, payload:)
         @type = type
-        @url  = payload['url']
+        set_payload_attributes(payload)
+      end
+
+      private
+
+      def set_payload_attributes(payload)
+        if @type == 'location'
+          @long = payload['coordinates']['long']
+          @lat  = payload['coordinates']['lat']
+        else
+          @url = payload['url']
+        end
       end
     end
   end
