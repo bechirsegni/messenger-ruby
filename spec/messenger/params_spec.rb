202f67e2a85bbcd96019ec6913ea03c6a17edf1a
mejcz <magda.wisniewska@netguru.co>
2016-04-25 20:41:20 +0200

    Add spec for Params#new

--- spec/messenger/params_spec.rb
+++ spec/messenger/params_spec.rb
@@ -1,6 +1,6 @@
 module Messenger
   describe Params do
-    let(:message_params) do
+    let(:params) do
       {
         "object" => "page",
         "entry" => [
@@ -9,18 +9,10 @@ module Messenger
             "time" => 1460967453657,
             "messaging" => [
               {
-                "sender" => {
-                  "id" => "5678"
-                },
-                "recipient" => {
-                  "id" => "9012"
-                },
+                "sender" => { "id" => "5678" },
+                "recipient" => { "id" => "9012" },
                 "timestamp" => 1460967453610,
-                "message" => {
-                  "mid" => "MID",
-                  "seq" => 1,
-                  "text" => "TEXT"
-                }
+                "message" => { "mid" => "MID", "seq" => 1, "text" => "TEXT" }
               }
             ]
           }
@@ -28,19 +20,23 @@ module Messenger
       }
     end
 
-    describe 'params with message' do
-      subject { described_class.new(message_params)}
+    subject { described_class.new(params)}
 
-      describe '#entries' do
-        it 'returns array of Entry objects' do
-          expect(subject.entries).to all(be_a(Parameters::Entry))
-        end
+    describe '#entries' do
+      it 'returns array of Entry objects' do
+        expect(subject.entries).to all(be_a(Parameters::Entry))
       end
+    end
 
-      describe '#first_entry' do
-        it 'returns Messaging object' do
-          expect(subject.first_entry).to be_a(Parameters::Messaging)
-        end
+    describe '#first_entry' do
+      it 'returns Messaging object' do
+        expect(subject.first_entry).to be_a(Parameters::Messaging)
+      end
+    end
+
+    describe '#new' do
+      it 'assigns raw params to params attribute' do
+        expect(subject.params).to eq(params)
       end
     end
   end
