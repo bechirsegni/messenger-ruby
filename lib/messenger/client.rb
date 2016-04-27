52b67dd41254363c2b24840110cfe4883d8c1184
justynjozwiak <justyn.jozwiak@netguru.pl>
2016-04-27 11:01:50 +0200

    client: fix wrong param

--- lib/messenger/client.rb
+++ lib/messenger/client.rb
@@ -2,7 +2,7 @@ module Messenger
   class Client
     def self.get_user_profile(user_id)
       JSON.parse(RestClient.get(
-        "https://graph.facebook.com/v2.6/#{fb_params.first_entry.sender_id}?fields=first_name,last_name,profile_pic \
+        "https://graph.facebook.com/v2.6/#{user_id}?fields=first_name,last_name,profile_pic \
         &access_token=#{Messenger.config.page_access_token}",
       ))
     end
